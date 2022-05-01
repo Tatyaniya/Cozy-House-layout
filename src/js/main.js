9ce982cd3fe99be66a355392862d087e3c812da0
Tatyaniya <afromyak13@gmail.com>
2022-05-02 00:00:48 +0300

    minify js

--- src/js/main.js
+++ src/js/main.js
@@ -14,17 +14,17 @@ const swiper = new Swiper('.swiper', {
     }
 });
 
-const anchors = document.querySelectorAll('a[href*="#"]')
+const anchors = document.querySelectorAll('a[href*="#"]');
 
 for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href').substr(1)
-    
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const blockID = anchor.getAttribute('href').substr(1);
+        
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
 }
